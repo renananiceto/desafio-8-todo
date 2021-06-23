2ac991149e67d37408bfc01a254f74ed86e07b11
renananiceto <renan.a.ferreira94@gmail.com>
2021-06-23 15:38:54 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+class App extends Component{
+
+    state = {
+        text:'',
+        list:[]
+    }
+     
+    remove= (ev, index) => {
+        this.setState({
+            list:this.state.list.filter((index) => index !== index)
+        })
+    };
+
+    handleChange= (ev) => {
+        this.setState({
+            text:ev.target.value   
+        })
+    };
+
+    clique= () => {
+        const { text, list } = this.state;
+        if(text.length > 0){
+            this.setState({
+                list: list.concat( text ), 
+            })         
+        }else{
+            alert('lista vazia')
+        }
+
+        this.setState( {
+            text: '',
+        } )
+    }; 
+
+
+    render() {
+        return(
+            <div>
+                <input onChange= {this.handleChange} type="text" value={this.state.text} placeholder='digite aqui' />
+                <button onClick={this.clique}>Ok</button>
+                {this.state.list.map((item,index) => (
+                   <div>
+                       <span> {item} </span>
+                       <button onClick={(ev) => this.remove(index)} >Remover</button>
+                   </div>
+                )) }
+            </div>
+        )
+    }
+};
 
 export default App;
